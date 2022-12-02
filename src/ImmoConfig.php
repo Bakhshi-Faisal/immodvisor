9ca40db8054ec29961a2d0fcd512e4d6da8b4bc9
Bakhshi-Faisal <afaisal_bakhshi@live.com>
2022-12-02 11:38:06 +0100

    feature: avoid errors

--- src/ImmoConfig.php
+++ src/ImmoConfig.php
@@ -1,50 +1,60 @@
 <?php
 namespace Bakhshi\Immodvisor;
 
+use JsonException;
+
 class ImmoConfig
 {
-    public function lastReviews($apiKey,$saltIn,$saltOut,$idCompany,$max){
+    /**
+     * @throws JsonException
+     */
+    public function lastReviews(string $apiKey, string $saltIn, string $saltOut, int $idCompany, int $max){
 
         $feedbacks = array();
-        $api = new Api($apiKey,
-            $saltIn,
-            $saltOut);
-        $api->env('prod');
-        $infoSociete = $api->companyGet();
+        if ((int)$idCompany !== 0) {
 
-        $api->debug(false);
-        $reviewsData = $api->reviewList($idCompany)->parse();
-        $brand_json = $api->companyGet($idCompany)->get();
-        $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
-        if (isset($brand["datas"]["company"]["rating"])){
-            $averageRating =isset($brand["datas"]["company"]["rating"]);
-            $averageRating = str_replace(".", ",", $averageRating);
-            $feedbacks = array_slice($reviewsData->datas->reviews,0,$max);
+            $api = new Api($apiKey,
+                $saltIn,
+                $saltOut);
+            $api->env('prod');
+            $infoSociete = $api->companyGet();
+
+            $api->debug(false);
+            $reviewsData = $api->reviewList($idCompany)->parse();
+            $brand_json = $api->companyGet($idCompany)->get();
+            $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
+            if (isset($brand["datas"]["company"]["rating"])) {
+                $averageRating = isset($brand["datas"]["company"]["rating"]);
+                $averageRating = str_replace(".", ",", $averageRating);
+                $feedbacks = array_slice($reviewsData->datas->reviews, 0, $max);
+            }
         }
-
         return array(
             'reviews' => $feedbacks
         );
     }
-    public function headerReviews($apiKey,$saltIn,$saltOut,$idCompany){
+    public function headerReviews(string $apiKey,string $saltIn,string $saltOut,int $idCompany){
 
-        $api = new Api($apiKey,
-            $saltIn,
-            $saltOut);
-        $api->env('prod');
-        $api->debug(false);
         $infoCompnay = array();
-        $reviewsData = $api->reviewList($idCompany ?? '')->parse();
-        $brand_json = $api->companyGet($idCompany ?? '')->get();
-        $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
-        if (isset($brand["datas"]["company"]["rating"])) {
-            $averageRating = $brand["datas"]["company"]["rating"];
-            $branchName = $brand["datas"]["company"]["address"]["city"];
-            $infoCompnay = array(
-                'rate' => $averageRating,
-                'branchName' => $branchName
-            );
+        if ((int)$idCompany !== 0) {
+            $api = new Api($apiKey,
+                $saltIn,
+                $saltOut);
+            $api->env('prod');
+            $api->debug(false);
 
+            $reviewsData = $api->reviewList($idCompany)->parse();
+            $brand_json = $api->companyGet($idCompany )->get();
+            $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
+            if (isset($brand["datas"]["company"]["rating"])) {
+                $averageRating = $brand["datas"]["company"]["rating"];
+                $branchName = $brand["datas"]["company"]["address"]["city"];
+                $infoCompnay = array(
+                    'rate' => $averageRating,
+                    'branchName' => $branchName
+                );
+
+            }
         }
         return $infoCompnay;
 
