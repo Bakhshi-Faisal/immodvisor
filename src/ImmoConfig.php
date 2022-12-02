5c9f0a50ed61545b9277ee34687b99291badd812
Bakhshi-Faisal <afaisal_bakhshi@live.com>
2022-12-02 10:16:44 +0100

    feature: avoid errors

--- src/ImmoConfig.php
+++ src/ImmoConfig.php
@@ -4,34 +4,49 @@ namespace Bakhshi\Immodvisor;
 class ImmoConfig
 {
     public function lastReviews($apiKey,$saltIn,$saltOut,$idCompany,$max){
+
+        $feedbacks = array();
         $api = new Api($apiKey,
             $saltIn,
             $saltOut);
         $api->env('prod');
+        $infoSociete = $api->companyGet();
 
         $api->debug(false);
-        $reviewsData = $api->reviewList($idCompany ?? '')->parse();
-        $brand_json = $api->companyGet($idCompany ?? '')->get();
-        $brand = json_decode($brand_json, TRUE);
-        $averageRating = $brand["datas"]["company"]["rating"];
-        $averageRating = str_replace(".", ",", $averageRating);
+        $reviewsData = $api->reviewList($idCompany)->parse();
+        $brand_json = $api->companyGet($idCompany)->get();
+        $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
+        if (isset($brand["datas"]["company"]["rating"])){
+            $averageRating =isset($brand["datas"]["company"]["rating"]);
+            $averageRating = str_replace(".", ",", $averageRating);
+            $feedbacks = array_slice($reviewsData->datas->reviews,0,$max);
+        }
 
         return array(
-            'reviews' => array_slice($reviewsData->datas->reviews,0,$max)
+            'reviews' => $feedbacks
         );
     }
     public function headerReviews($apiKey,$saltIn,$saltOut,$idCompany){
+
         $api = new Api($apiKey,
             $saltIn,
             $saltOut);
         $api->env('prod');
         $api->debug(false);
+        $infoCompnay = array();
         $reviewsData = $api->reviewList($idCompany ?? '')->parse();
         $brand_json = $api->companyGet($idCompany ?? '')->get();
-        $brand = json_decode($brand_json, TRUE);
-        $averageRating = $brand["datas"]["company"]["rating"];
-        
-        return $averageRating;
-        
+        $brand = json_decode($brand_json, TRUE, 512, JSON_THROW_ON_ERROR);
+        if (isset($brand["datas"]["company"]["rating"])) {
+            $averageRating = $brand["datas"]["company"]["rating"];
+            $branchName = $brand["datas"]["company"]["address"]["city"];
+            $infoCompnay = array(
+                'rate' => $averageRating,
+                'branchName' => $branchName
+            );
+
+        }
+        return $infoCompnay;
+
     }
 }
